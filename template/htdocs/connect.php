ad72ef3a786ad009c9e18a8856c2d394798997f6
antradar <netops@antradar.com>
2014-03-30 23:28:46 -0400

    socket connection

--- template/htdocs/connect.php
+++ template/htdocs/connect.php
@@ -1,3 +1,6 @@
 <?
 include_once "sql.php";
 $db=sql_get_db('localhost','gyrostart','root','mnstudio');
+
+// use socket instead of TCP if the database is on the same, Linux server
+// $db=sql_get_db(':/var/run/mysqld/mysqld.sock','gyrostart','root','mnstudio');
