f0c6daeac6dbc68df1b0c5951db61433ac71e8c3
antradar <netops@antradar.com>
2014-03-30 23:07:03 -0400

    4.5.1
    using mysqli now that it is supported by HHVM

--- template/htdocs/sql.php
+++ template/htdocs/sql.php
@@ -1,39 +1,35 @@
 <?
 // Macronetic Hydra
 // Common SQL Interface
-// MySQL implementation
-// (c) Antradar Software 1995-2006
+// MySQLi implementation
+// (c) Antradar Software 1995-2013
 
-$SQL_ENGINE="MySQL";
+$SQL_ENGINE="MySQLi";
 
 function sql_get_db($dbhost,$dbsource,$dbuser,$dbpass){
-	$db=mysql_connect($dbhost,$dbuser,$dbpass);
-	mysql_select_db($dbsource,$db);
+	$db=mysqli_connect($dbhost,$dbuser,$dbpass,$dbsource);
 	return $db;
 }
 
 function sql_query($query,$db){
-	$rs=mysql_query($query,$db);
+	$rs=mysqli_query($db,$query);
 	if (!$rs) echo "sql_error: ".$query;
 	return $rs;
 }
 
 function sql_fetch_array($rs){
-	return mysql_fetch_array($rs);
-}
+	return mysqli_fetch_array($rs);
 
-function sql_fetch_assoc($rs){
-	return mysql_fetch_assoc($rs);
 }
 
 function sql_insert_id($db,$rs=null){
 
 	if (!isset($rs)) return mysql_insert_id();
-	return mysql_insert_id($db);
+	return mysqli_insert_id($db);
 }
 
 function sql_affected_rows($db,$rs){
-	return mysql_affected_rows($db);
+	return mysqli_affected_rows($db);
 }
 
 function sql_begin_transaction(){
@@ -48,25 +44,3 @@ function sql_rollback(){
 	die("sql.ard: not implemented!");
 }
 
-/* Sample Use
-
-$db=sql_get_db("localhost","mnhydra","root","mnstudio");
-$query="select * from pages";
-$rs=sql_query($query,$db);
-echo sql_affected_rows($db)." rows affected <br>";
-
-
-  while ($myrow=sql_fetch_array($rs)){
-	$pagefilename=$myrow['pagefilename'];
-	$pageid=$myrow['pageid'];
-	$pagetype=$myrow['pagetype'];
-	$idepath="./ide/";
-	if ($pagetype>1) $pagefilename=$idepath.$pagefilename;
-
-	echo $pageid." ".$pagefilename."<br>";
-  }
-
-*/
-
-
-?>
