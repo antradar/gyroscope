59e0bf0678302ba845f13a5be9b235b3b8eb93e0
unknown <schien@antradar.com>
2022-11-23 17:17:23 -0500

    ui mode switced back

--- template/htdocs/uiconfig.php
+++ template/htdocs/uiconfig.php
@@ -1,9 +1,9 @@
 <?php
 
 $uiconfig=array(
-	'toolbar_position'=>'left', //default: top; options: top, left; when set to left, the tab list takes over the toolbar and shares the space with a master search box
+	'toolbar_position'=>'top', //default: top; options: top, left; when set to left, the tab list takes over the toolbar and shares the space with a master search box
 	'closeall_button'=>'before', //default: place CloseAll button before all the tabs; "after" places the CloseAll button after the Home tab.
-	'enable_master_search'=>true, //default: false; when enabled, the tab list takes over the tool bar and share the space with a master searchbox
+	'enable_master_search'=>false, //default: false; when enabled, the tab list takes over the tool bar and share the space with a master searchbox
 	'force_inline_lookup'=>false, //default: false; use side lookup panel when possible
 );
 
