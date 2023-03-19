21eecb97d54395355e9517023c254d67a07f642a
unknown <schien@antradar.com>
2023-03-19 01:29:27 -0400

    mega uploaders; adaptive chunksize for small uploads

--- template/htdocs/uploaderstyle.php
+++ template/htdocs/uploaderstyle.php
@@ -1,3 +1,4 @@
+#cancel{display:none;font-size:12px;padding:5px 0;}
 #progress_{vertical-align:middle;display:inline-block;border:solid 1px #999999;border-radius:3px;overflow:hidden;width:85%;}
 #progress{display:inline-block;background:#ffff00;height:12px;}
 #cancelbutton{vertical-align:middle;display:inline-block;padding:1px 4px;background:#ab0200;color:#ffffff;font-size:11px;cursor:pointer;border-radius:50%;font-family:Arial,sans-serif;}
