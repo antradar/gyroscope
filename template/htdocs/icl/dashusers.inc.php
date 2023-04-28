e73b786066415ecb20a4df7860e160a5a860760e
unknown <schien@antradar.com>
2023-04-28 18:39:05 -0400

    to-list icon in users view hidden in doc-centric view

--- template/htdocs/icl/dashusers.inc.php
+++ template/htdocs/icl/dashusers.inc.php
@@ -3,13 +3,20 @@
 include 'icl/listusers.inc.php';
 
 function dashusers(){
+	global $uiconfig;
 	
 	header('tabctx: subdash');
 	header('newtitle: Users');
 ?>
 <div class="section userdash">
-	
+
+	<?php
+	if ($uiconfig['toolbar_position']=='top'){
+	?>	
 	<span class="dash_to_list" onclick="dash_to_quicklist();">&mapstoleft;</span>
+	<?php
+	}
+	?>
 		
 	<div class="sectiontitle">Users</div>
 	
