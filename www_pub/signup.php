0f77b9804c381c9297de04d71c3c44e9f10edf9d
unknown <schien@antradar.com>
2024-08-18 01:02:32 -0400

    layout change

--- www_pub/signup.php
+++ www_pub/signup.php
@@ -19,33 +19,33 @@ show_web_login_header();
 	<form id="signupform" method="POST" style="padding:20px;margin:0;padding-top:0px;" onsubmit="return checksignupform();">
 
 		<div style="padding-top:10px;"><label for="username">Your Name:</label></div>
-		<div style="padding-top:5px;padding-bottom:15px;">
+		<div style="padding-top:5px;padding-bottom:10px;">
 			<input style="width:100%;" class="lfinp" id="username" type="text" autocomplete="off">
 		</div>				
 		
 		<div style="padding-top:10px;"><label for="companyname">Company:</label></div>
-		<div style="padding-top:5px;padding-bottom:15px;">
+		<div style="padding-top:5px;padding-bottom:10px;">
 			<input style="width:100%;" class="lfinp" id="companyname" type="text" autocomplete="off">
 		</div>
 	
 		<div style="padding-top:10px;"><label for="useremail">Email:</label></div>
-		<div style="padding-top:5px;padding-bottom:15px;">
+		<div style="padding-top:5px;padding-bottom:10px;">
 			<input style="width:100%;" class="lfinp" id="useremail" type="text" autocomplete="off">
 		</div>
 		
 		<div style="padding-top:10px;"><label for="userphone">Phone:</label></div>
-		<div style="padding-top:5px;padding-bottom:15px;">
+		<div style="padding-top:5px;padding-bottom:10px;">
 			<input style="width:100%;" class="lfinp" id="userphone" type="text" autocomplete="off">
 		</div>		
 		
 		<div><label for="password">Password:</label></div>
-		<div style="padding-top:5px;padding-bottom:15px;position:relative;">
+		<div style="padding-top:5px;padding-bottom:10px;position:relative;">
 			<input style="width:100%;" class="lfinp" id="password" type="password" name="password" autocomplete="off">
 			<img class="passtoggle" onclick="togglepass_login(this,'password','app/');" src="app/imgs/eye-slash.png">
 		</div>
 		
 		<div><label for="password">Retype Password:</label></div>
-		<div style="padding-top:5px;padding-bottom:15px;position:relative;">
+		<div style="padding-top:5px;padding-bottom:10px;position:relative;">
 			<input style="width:100%;" class="lfinp" id="password2" type="password" name="password2" autocomplete="off">
 			<img class="passtoggle" onclick="togglepass_login(this,'password2','app/');" src="app/imgs/eye-slash.png">
 		</div>		
